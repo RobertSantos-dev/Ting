84c04f19bb10d9a2b3ff8089156a849e53f4849d
Robert Santos <robertdosantos09@gmail.com>
2023-05-08 15:42:43 -0300

    feat: Adicionando codigos a class Queue. Para o Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._list_row = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._list_row)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._list_row.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if (len(self._list_row) == 0):
+            return None
+        return self._list_row.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        try:
+            if (index < 0):
+                raise IndexError
+            return self._list_row[index]
+        except IndexError:
+            raise IndexError("Índice Inválido ou Inexistente")
