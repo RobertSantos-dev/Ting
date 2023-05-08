1f60d8ed90d8a4ac6ec90b1ccb97e5a554510c29
Robert Santos <robertdosantos09@gmail.com>
2023-05-08 20:42:54 -0300

    feat: Adicionando codigos a função file_metadata. Para o requisito 5.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -27,4 +27,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        element_search = instance.search(position)
+    except IndexError:
+        sys.stderr.write('Posição inválida')
+    else:
+        sys.stdout.write(str(element_search))
