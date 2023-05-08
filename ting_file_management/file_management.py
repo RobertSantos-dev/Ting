027b05054d1501a2c43f90c2fb22feddc47e3b3e
Robert Santos <robertdosantos09@gmail.com>
2023-05-08 17:16:12 -0300

    feat: Adicionando codigos a função txt_importer. Para o requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    try:
+        if (path_file[-4:] != '.txt'):
+            raise FileExistsError
+
+        with open(path_file) as file:
+            file_txt = file.read().split('\n')
+            return file_txt
+
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    except FileExistsError:
+        sys.stderr.write('Formato inválido')
