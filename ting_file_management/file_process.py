5a4dbfe473bba7d6c70fb301f00ff9d064ce4775
Robert Santos <robertdosantos09@gmail.com>
2023-05-08 19:07:44 -0300

    feat: Adicionando codigos a função process. Para o requisito 3.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for value in instance.list_row:
+        if (value['nome_do_arquivo'] == path_file):
+            return
+    list_row_file = txt_importer(path_file)
+    new_dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(list_row_file),
+        'linhas_do_arquivo': list_row_file
+    }
+    instance.enqueue(new_dict)
+    sys.stdout.write(str(new_dict))
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,23 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self._list_row = []
+        self.list_row = []
 
     def __len__(self):
-        return len(self._list_row)
+        return len(self.list_row)
 
     def enqueue(self, value):
-        self._list_row.append(value)
+        self.list_row.append(value)
 
     def dequeue(self):
-        if (len(self._list_row) == 0):
+        if (len(self.list_row) == 0):
             return None
-        return self._list_row.pop(0)
+        return self.list_row.pop(0)
 
     def search(self, index):
         try:
             if (index < 0):
                 raise IndexError
-            return self._list_row[index]
+            return self.list_row[index]
         except IndexError:
             raise IndexError("Índice Inválido ou Inexistente")
