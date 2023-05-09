1fa1b753ef3a480ab374dd6a75c7619dc54664aa
Robert Santos <robertdosantos09@gmail.com>
2023-05-08 23:12:02 -0300

    feat: Adicionando codigos a função exists_word. Para o requisito 7.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,16 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_file = instance.list_row[0]
+    list_result = [{
+        'palavra': word,
+        'arquivo': list_file['nome_do_arquivo'],
+        'ocorrencias': [],
+    }]
+    for i in range(len(instance.list_row[0]['linhas_do_arquivo'])):
+        if (word in list_file['linhas_do_arquivo'][i].lower()):
+            list_result[0]['ocorrencias'].append({'linha': i + 1})
+    if (not list_result[0]['ocorrencias']):
+        return []
+    return list_result
 
 
 def search_by_word(word, instance):
